08ae89bfd7adafe8d951ddee11edfb6f6bfa8a3e
ajur26 <andrzej.w.jur@gmail.com>
2026-08-07 11:59:31 +0200

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/organise/me/OrganisemeApplication.java
@@ -0,0 +1,13 @@
+package com.organise.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganisemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganisemeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/organise/me/OrganisemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organise.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganisemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
