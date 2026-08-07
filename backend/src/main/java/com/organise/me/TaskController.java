6fd84b3e299ce79faeeb55657ee0c676303add4e
ajur26 <andrzej.w.jur@gmail.com>
2026-08-07 13:11:40 +0200

    Add task API

--- backend/src/main/java/com/organise/me/TaskController.java
+++ backend/src/main/java/com/organise/me/TaskController.java
@@ -1,21 +1,30 @@
 package com.organise.me;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
 
 public class TaskController {
+    
+    private final List<Task> tasks = new ArrayList<>();
+
+    public TaskController() {
+        tasks.add(new Task(1L, "Task 1", "Description for task 1", false));
+        tasks.add(new Task(2L, "Task 2", "Description for task 2", true));
+    }
 
     @GetMapping
     public List<Task> getTasks() {
-        return List.of(new Task(1L, "Task 1", "Description for Task 1", false),
-                new Task(2L, "Task 2", "Description for Task 2", true),
-                new Task(3L, "Task 3", "Description for Task 3", false)
-            );
+        return tasks;
+    }
+
+    @PostMapping
+    public Task addTask(@RequestBody Task task){
+        tasks.add(task);
+        return task;
     }
 }
